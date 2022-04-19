1a6652fe006e8c6a2bc1e067ba08df479de0ab78
JostesMarcel <jostes77@googlemail.com>
2022-04-19 16:57:11 +0200

    new api.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,3 +1,5 @@
-process.env.NODE_ENV === "production" 
+const api_url = process.env.NODE_ENV === "production" 
                 ? "https://kino-booker-backend.herokuapp.com/"
                 : "http://localhost:9000"
+
+export default api_url
